ed40783caf4e449f05d5ef799b08fcf3117cea0b
Ritesh Mehrotra <riteshmehrotra@gmail.com>
2021-07-29 07:42:08 +0530

    Reorder menu

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -26,6 +26,7 @@ const LoggedInMenu = ({id}) => {
 
     return <Menu stackable inverted>
         <Link href="/"><Menu.Item onClick={() => onClickMenu("Dashboard", "connectdevelop")} active={isActive("/", router.pathname)} ><h2><Icon color='orange' size='large' className={styles.Applogo} name='connectdevelop'></Icon>Nexus</h2></Menu.Item></Link>
+        <Link href={`/directory/${id}`}><Menu.Item onClick={() => onClickMenu("KrisMatch", "expand arrows alternate")} active={isActive(`/directory${id}`, router.pathname)}><Icon color='red' name='expand arrows alternate'></Icon>KrisMatch</Menu.Item></Link>
         <Link href="/lab"><Menu.Item onClick={() => onClickMenu("Lab", "lab")} active={isActive("/lab", router.pathname)}><Icon color='yellow' name='lab'></Icon>Lab</Menu.Item></Link>
         {/* <Link href="/insights"><Menu.Item onClick={() => onClickMenu("Insights", "eye")} active={isActive("/insights", router.pathname)}><Icon color='red' name='eye'></Icon>Insights</Menu.Item></Link> */}
         {/* <Link href="/challenges"><Menu.Item active={isActive("/challenges", router.pathname)}><Icon color='green' name='chess board'></Icon>Challenges</Menu.Item></Link> */}
@@ -34,7 +35,6 @@ const LoggedInMenu = ({id}) => {
         <Link href="/events"><Menu.Item onClick={() => onClickMenu("Events", "calendar alternate outline")} active={isActive("/events", router.pathname)}><Icon color='olive' name='calendar alternate outline'></Icon>Events</Menu.Item></Link>
         <Link href="/references"><Menu.Item onClick={() => onClickMenu("References", "newspaper outline")} active={isActive("/references", router.pathname)}><Icon color='teal' name='newspaper outline'></Icon>References</Menu.Item></Link>
         <Link href="/rewards"><Menu.Item onClick={() => onClickMenu("Rewards", "star")} active={isActive("/rewards", router.pathname)}><Icon color='green' name='star'></Icon>Rewards</Menu.Item></Link>
-        <Link href={`/directory/${id}`}><Menu.Item onClick={() => onClickMenu("KrisMatch", "expand arrows alternate")} active={isActive(`/directory${id}`, router.pathname)}><Icon color='red' name='expand arrows alternate'></Icon>KrisMatch</Menu.Item></Link>
         <Menu.Menu position='right'>
             <Link href={`/profiles/${id}?showNotifications=true`}><Menu.Item onClick={() => onClickMenu("My profile", "star")} active={isActive(`/profiles/${id}`, router.pathname)} position='right'><Icon color='red' name='bell'></Icon>Notifications<Label color='red' circular>
                 2
